07e191c71ea39540c68d247181de904d860feead
mbrenoaf <123556594+mbrenoaf@users.noreply.github.com>
2023-02-11 20:39:24 -0300

    Add files via upload

---
+++ desafio_area.js
@@ -0,0 +1,8 @@
+// PI * raio * raio
+
+const PI = 3.14159265359
+const raio = 10 
+const area = PI * raio * raio
+console.log('A circunferência da área é: ', area +'m2')
+
+Math.PI //PI em Javascript
